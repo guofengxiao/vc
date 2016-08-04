f6e36c9d177c27693d2a449bb0be53f3cdab50bc
Ifan <dingfengbosushi@gmail.com>
2016-08-04 10:24:03 +0800

    "1 add ons " and "2 manuals"
    create add on:
    areaCheck
    create manual:
    vcMotionInterpolator
    vcMotionTarget
    vcRslProgramExecutor

---
+++ manual/vcRslProgramExecutor/vcRslProgramExecutor.py
@@ -0,0 +1,14 @@
+'vcRslProgramExecutor executes simple motion and control sequences of robots and manipulators.'
+'RSL (Robot Sequence Language) consists of 3 levels: Program, Routine and a Statement. '
+'It is simple and limited in the sense that it does not have any control statements such as if-then-else or for loops but because of the very same reason it is extremely easy to program—see RSL Program Executor.'
+
+RSL (Robot Sequence Language)
+机器人顺序语言
+Program
+  Routine
+    Statement
+  
+
+继承关系
+vcBehaviour
+  vcRslProgramExecutor
